22aed99bf43125b374dcf2817d815bb7297c2e43
fer-xavmc2004 <nandoxavier2004@gmail.com>
2026-01-03 03:48:52 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,9 @@
-const CACHE_NAME = 'pecheras-universal-v1';
+const CACHE_NAME = 'v-gen-1';
 const ASSETS = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', (e) => {
     e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)));
     self.skipWaiting();
-});
-
-self.addEventListener('activate', (e) => {
-    e.waitUntil(caches.keys().then((ks) => Promise.all(ks.map((k) => k !== CACHE_NAME && caches.delete(k)))));
-    return self.clients.claim();
 });
 
 self.addEventListener('fetch', (e) => {
